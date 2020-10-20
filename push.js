9b4c3249e774b1346d3c3366e2750c8ca8209f21
anggadwi16 <a123aku@gmail.com>
2020-10-20 20:20:47 +0700

    update push.js dan hosting.txt

--- push.js
+++ push.js
@@ -14,11 +14,11 @@ webPush.setVapidDetails(
 
 let pushSubcription = {
 
-	"endpoint" : " https://fcm.googleapis.com/fcm/send/d7exMWtiu_k:APA91bHgw2b-nfBWwZYLrn1qqO_z_ZJlC4dti-yf4Siv9kc7X9KVNiBfIsnsQlNyrAGyP5oGANL6hYYiCl7_72IE_gfnd6cARbR-Kl4hdNRm1BntFWo-HqBQjgfZ-djrfR6VXwnzW6a1",
+	"endpoint" : " https://fcm.googleapis.com/fcm/send/cb1A6VlC6Oc:APA91bFChJxcDsyuC8-j6pyht-xing252fcgdz3ruEvlwO2m3yLV6dVr8pRWLvsPxoDYG6L_q7fZ2JtLxue4LAAuqeJGdvjM6IP2DaFm2z8oBPbU1YtKxgAct7JqnHAXO6uhpo91epKe",
 	"keys" : {
 
-		"p256dh" : "BDCeZ7kspMcNTgVM/D22pjH5Ql0cYB1kXpgbK4vhDYcuDXoDIYdyPmea8UIUN+R5Ad1zNykKATp36rohSarFJ0A=",
-		"auth" : "wdC8fJWMaVwjDngTKTtm+A=="
+		"p256dh" : "BBbXD6EEApAYY/2W5+bEj6S+NVHUrLi511F5cYj1VMdWuCu1BdGDfM/5ZNME6votwtIGKWRO9KM+Bp6N00UB6EM=",
+		"auth" : "gi9Z3V3oxkn04noUlIVXtA=="
 	}
 
 };
